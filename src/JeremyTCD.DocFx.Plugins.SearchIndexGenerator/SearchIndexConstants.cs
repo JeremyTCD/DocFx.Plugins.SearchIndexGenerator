25f741a1eb1139091526841470034dfc6b56e2d2
JeremyTCD <jeremytanchongdao@gmail.com>
2017-12-21 16:48:23 +0800

    Renamed option prefix to jtcd.

--- src/JeremyTCD.DocFx.Plugins.SearchIndexGenerator/SearchIndexConstants.cs
+++ src/JeremyTCD.DocFx.Plugins.SearchIndexGenerator/SearchIndexConstants.cs
@@ -2,8 +2,8 @@
 {
     public static class SearchIndexConstants
     {
-        public const string SearchIndexSnippetLengthKey = "jr.SearchIndexSnippetLength";
-        public const string IncludeInSearchIndexKey = "jr.includeInSearchIndex";
+        public const string SearchIndexSnippetLengthKey = "jtcd.SearchIndexSnippetLength";
+        public const string IncludeInSearchIndexKey = "jtcd.includeInSearchIndex";
         public const string IndexFileName = "index.json";
         public const int DefaultArticleSnippetLength = 500;
     }
