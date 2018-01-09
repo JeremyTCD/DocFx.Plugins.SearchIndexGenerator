96274d7a1d19d2afb0fec5fcb2626311657c2f04
JeremyTCD <jeremytanchongdao@gmail.com>
2018-01-09 18:43:34 +0800

    Updated constants
    - Use _ instead of . so that model variables are easily accessible via docfx's js api

--- src/JeremyTCD.DocFx.Plugins.SearchIndexGenerator/SearchIndexConstants.cs
+++ src/JeremyTCD.DocFx.Plugins.SearchIndexGenerator/SearchIndexConstants.cs
@@ -2,8 +2,8 @@
 {
     public static class SearchIndexConstants
     {
-        public const string SearchIndexSnippetLengthKey = "jtcd.searchIndexSnippetLength";
-        public const string IncludeInSearchIndexKey = "jtcd.includeInSearchIndex";
+        public const string SearchIndexSnippetLengthKey = "jtcd_searchIndexSnippetLength";
+        public const string IncludeInSearchIndexKey = "jtcd_includeInSearchIndex";
         public const string IndexFileName = "index.json";
         public const int DefaultArticleSnippetLength = 500;
     }
